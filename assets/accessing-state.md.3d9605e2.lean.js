45082245a60c89938fa4bb3670e87b24b1a5f8b7
Ernesto Canquiz <fdsoil123@gmail.com>
2023-04-19 20:39:42 -0400

    deploy

---
+++ assets/accessing-state.md.3d9605e2.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o,N as e}from"./chunks/framework.23a4abfb.js";const u=JSON.parse('{"title":"Accediendo al Estado","description":"","frontmatter":{},"headers":[],"relativePath":"accessing-state.md"}'),n={name:"accessing-state.md"},l=e("",48),p=[l];function t(c,r,d,i,y,F){return o(),a("div",null,p)}const A=s(n,[["render",t]]);export{u as __pageData,A as default};
